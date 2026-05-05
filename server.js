7eb4b60b3b4ab8363d64bb01970d1c86d59c5625
sm-sos <omar@sadeed-em.com>
2026-05-05 19:23:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("SM-SOS API Running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
